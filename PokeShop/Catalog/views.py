bc055c812f579b9d05ffa099af295a456a2d8738
getaudio2 <paugermaisar@hotmail.es>
2025-03-24 18:27:52 +0100

    Added GET and POST product api_view

--- PokeShop/Catalog/views.py
+++ PokeShop/Catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from Catalog.models import Categoria, Product
+from Catalog.serializers import CategoriaSerializer, ProductSerializer
+
+@api_view(['GET'])
+def getProductos(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createProducto(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
