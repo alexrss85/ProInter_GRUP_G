c9b8ae4d68a8291f518b42594a1cb0cb04aa609e
Victor Fernandez <fernandezvictor738@gmail.com>
2025-03-24 17:49:23 +0100

    cambios en los modelos de cart y catalog

--- PokeShop/Catalog/models.py
+++ PokeShop/Catalog/models.py
@@ -1,16 +1,12 @@
 from django.db import models
 
-class Categoria(models.Model):
-    nom = models.CharField(max_length=50) 
-
 # Create your models here.
 class Categoria(models.Model):
     nom = models.CharField(max_length=50) 
-    
+
 class Product(models.Model):
     nom = models.CharField(max_length=50)  
     descripcio = models.CharField(max_length=50) 
     preu = models.IntegerField()  
     stock = models.IntegerField()  
     nom_categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    rating = models.IntegerField()
