131c7bcfd526f87136e40b6dfa4ee1c0ce54ddca
getaudio2 <paugermaisar@hotmail.es>
2025-03-24 17:43:51 +0100

    init paul

--- PokeShop/Catalog/models.py
+++ PokeShop/Catalog/models.py
@@ -2,10 +2,6 @@ from django.db import models
 
 class Categoria(models.Model):
     nom = models.CharField(max_length=50, unique=True) 
-
-# Create your models here.
-class Categoria(models.Model):
-    nom = models.CharField(max_length=50) 
     
 class Product(models.Model):
     nom = models.CharField(max_length=50)  
