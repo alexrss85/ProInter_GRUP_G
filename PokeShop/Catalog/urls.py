8b9bd5bc60597acad64c30ef2e0d242ab4feed48
Victor Fernandez <fernandezvictor738@gmail.com>
2025-03-26 20:31:06 +0100

    CRUD categoria

--- PokeShop/Catalog/urls.py
+++ PokeShop/Catalog/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('categorias/create/', views.create_categoria, name='create_categoria'),
-]
-from Catalog.views import getProductos, createProducto
+from Catalog.views import getProductos, createProducto, create_categoria, list_categorias, update_categoria, delete_categoria
 
 urlpatterns = [
     path('productos/', getProductos, name='get_productos'),
     path('create_producto/', createProducto, name='create_producto'),
+    path('categorias/create/', create_categoria, name='create_categoria'),
+    path('categorias/', list_categorias, name='list_categorias'),
+    path('categorias/update/<int:pk>/', update_categoria, name='update_categoria'), 
+    path('categorias/delete/<int:pk/', delete_categoria, name='delete_categoria'),
 ]
--- PokeShop/Catalog/views.py
+++ PokeShop/Catalog/views.py
@@ -30,3 +30,31 @@ def create_categoria(request):
         serializer.save()
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
+
+@api_view(['GET'])
+def list_categorias(request):
+    categorias = Categoria.objects.all()
+    serializer = CategoriaSerializer(categorias, many=True)
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def update_categoria(request, pk):
+    try:
+        categoria = Categoria.objects.get(pk=pk)
+    except Categoria.DoesNotExist:
+        return Response(status=404)
+
+    serializer = CategoriaSerializer(categoria, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(['DELETE'])
+def delete_categoria(request, pk):
+    try:
+        categoria = Categoria.objects.get(pk=pk) 
+        categoria.delete()
+        return Response(status=204)  
+    except Categoria.DoesNotExist:
+        return Response({'detail': 'Categoria no encontrada.'}, status=404)
