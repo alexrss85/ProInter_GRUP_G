ff3bb62fdd04db343847908f736ad4dab4a97271
getaudio2 <paugermaisar@hotmail.es>
2025-03-31 18:47:13 +0200

    GetOrders and addCartItemsToOrder endpoints

--- PokeShop/Order/views.py
+++ PokeShop/Order/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from Cart.models import User, Carrito, ItemCarrito
+from Catalog.models import Product
+from Order.models import Order, ItemOrder
+from .serializers import OrderSerializer, ItemOrderSerializer
+from rest_framework import status
 
-# Create your views here.
+@api_view(['GET'])
+def getOrdersByUser(request, user_id):
+    orders = Order.objects.filter(id_usuari_id=user_id)
+
+    if not orders.exists():
+        return Response({"error": "No orders found for this user."}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def addCartItemsToOrder(request):
+    user_id = request.data.get('user_id')
+
+    if not user_id:
+        return Response({"error": "User ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.filter(id=user_id).first()
+    if not user:
+        return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    cart = Carrito.objects.filter(user_id=user).first()
+    if not cart:
+        return Response({"error": "Cart not found."}, status=status.HTTP_404_NOT_FOUND)
+
+    cart_items = ItemCarrito.objects.filter(cart_id=cart)
+    if not cart_items.exists():
+        return Response({"error": "No items in cart."}, status=status.HTTP_400_BAD_REQUEST)
+
+    order = Order.objects.create(preu_total=0, estat="Pending", user_id=user)
+
+    total_price = 0
+    for item in cart_items:
+        ItemOrder.objects.create(order_id=order, product_id=item.product_id, quantity=item.quantity)
+        total_price += item.product_id.preu * item.quantity
+
+    order.preu_total = total_price
+    order.save()
+
+    serializer = OrderSerializer(order)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
