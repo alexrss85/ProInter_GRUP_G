cbd571fde3d7568e836297265b8e43c88e1206cc
getaudio2 <paugermaisar@hotmail.es>
2025-03-31 18:45:48 +0200

    Changed order and cart models attribute names

--- PokeShop/Order/models.py
+++ PokeShop/Order/models.py
@@ -3,10 +3,12 @@ from Cart.models import User
 from Catalog.models import Product
 
 class Order(models.Model):
-    preu_total = models.IntegerField(max_length=50)  
+    preu_total = models.IntegerField()  
     estat = models.CharField(max_length=50) 
-    id_usuari = models.ForeignKey(User, on_delete=models.CASCADE)  
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)  
 
 class ItemOrder(models.Model):
-    oder_id = models.ForeignKey(Order, on_delete=models.CASCADE)  
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE) 
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)  
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
