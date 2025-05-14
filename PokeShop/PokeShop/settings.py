e174e6bea304af8a5d8ae322611c2f53e8f74ddb
alexrss85 <2023_alex.rosendo@iticbcn.cat>
2025-05-14 20:19:27 +0200

    cambios

--- PokeShop/PokeShop/settings.py
+++ PokeShop/PokeShop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'Order',
     'Payment',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'PokeShop.urls'
 
